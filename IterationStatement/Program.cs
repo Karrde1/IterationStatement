44eb19d73ff7f8a9a6d9c197862de79989829029
Gregory Gauthier <ggauthier@mail.usf.edu>
2019-01-21 11:56:02 -0500

    This is a commit for the changes

--- IterationStatement/Program.cs
+++ IterationStatement/Program.cs
@@ -6,7 +6,39 @@ namespace IterationStatement
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter an Integer Value Between 1 and 100 ");
+            /* Using the Try-catch Block To Validate UserInput
+             * If the user provides bad input, the catch block 
+             * will handle the error and amessage will be displayed.
+             */
+
+            try
+            {
+                string input = Console.ReadLine();
+
+                int InputValue = int.Parse(input);
+
+                while (InputValue > 0 && InputValue <= 100)
+                {
+                    Console.WriteLine("This will Execute a While Loop");
+
+                    Console.WriteLine("You Have Entered " + InputValue.ToString() + ". This is the current integer value in the loop: #" + InputValue--);
+                }
+
+                Console.WriteLine("Press any key to exit the program...");
+                Console.ReadKey(true);
+            } // End of Try
+
+            catch
+            {
+                Console.WriteLine("Please enter an Integer Value and Try again");
+                Console.WriteLine("Press any key to exit the program...");
+                Console.ReadKey(true);
+
+
+
+            }
+
         }
     }
 }
